d81253e50d8464460d03830b48acc9ccf4509742
GiuseppeIaconog <GiuseppeIaconog@gmail.com>
2012-06-22 11:56:43 +0000

    1.0.0 RC 3

--- src/main/java/com/fides/GrinderTCPProxy.java
+++ src/main/java/com/fides/GrinderTCPProxy.java
@@ -1,3 +1,17 @@
+//   Copyright 2012 Giuseppe Iacono, Felipe Munoz Castillo
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 package com.fides;
 
 import java.io.File;
@@ -38,8 +52,6 @@ import net.grinder.util.AttributeStringParserImplementation;
 import net.grinder.util.SimpleStringEscaper;
 import net.grinder.util.http.URIParserImplementation;
 
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.picocontainer.DefaultPicoContainer;
 import org.picocontainer.PicoContainer;
 import org.picocontainer.behaviors.Caching;
